ce7cad0b49a27d069ec3a82dfdbc0cba3f1bb3b2
sunny <sunnykumarthakur29907@gmail.com>
2025-03-19 22:58:09 +0530

    Monthly report added using AI

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,11 +2,18 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
 import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
   processRecurringTransaction,
   triggerRecurringTransactions,
 } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processRecurringTransaction, triggerRecurringTransactions],
+  functions: [
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
+    checkBudgetAlerts,
+  ],
 });
